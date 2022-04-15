3e48094c746f8e08755659b8cbaf625449ee33b5
Xorikita <sedrin880@gmail.com>
2022-04-15 16:24:36 +0300

    send email KOI-8

--- scripts/send.php
+++ scripts/send.php
@@ -1,49 +1,66 @@
-<?php
-
-// if(isset($_POST['submit'])){
-// $to = "sedrin880@gmail.com";; // Здесь нужно написать e-mail, куда будут приходить письма
-// $from = $_POST['email']; // this is the sender's Email address
-// $phone = $_POST['phone'];
-// $subject = "adssmart";
-// $subject2 = "Copy of your form submission";
-// $message = $from . " оставил сообщение:" . "\n\n" . $_POST['message'] . "\n\n" . "Контактный телефон отправителя:" . $phone;
-// $message2 = "Here is a copy of your message " . $first_name . "\n\n" . $_POST['message'];
-
-// $headers = "From:" . $from;
-// $headers2 = "From:" . $to;
-
-// mail($to,$subject,$message,$headers);
-// // mail($from,$subject2,$message2,$headers2); // sends a copy of the message to the sender - Отключено!
-// echo "Сообщение отправлено. Спасибо Вам " . $first_name . ", мы скоро свяжемся с Вами.";
-// echo "<br /><br /><a href='http://adssmart.ru/index.html'>Вернуться на сайт.</a>";
-
-// }
-
-?>
-
-
-<!--Переадресация на главную страницу сайта, через 3 секунды-->
-<!-- <script language="JavaScript" type="text/javascript">
-function changeurl(){eval(self.location="http://adssmart.ru/index.html");}
-window.setTimeout("changeurl();",3000);
-</script> -->
-
 <?
-if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) && !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest' && !empty($_POST['email'])) {
-    $message = 'Email: ' . $_POST['email'] . ' ';
+if(isset($_SERVER['HTTP_X_REQUESTED_WITH']) 
+&& !empty($_SERVER['HTTP_X_REQUESTED_WITH']) 
+&& strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) 
+== 'xmlhttprequest' 
+&& !empty($_POST['email'])) {
+    $mailfrom = 'Email: ' . $_POST['email'] . ' ';
     $message .= 'Телефон: ' . $_POST['phone'] . "\n";
     if(!empty($_POST['message'])) {
         $message .= 'Сообщение: ' . $_POST['message'] . ' ';
     }
-    $mailTo = "sedrin880@gmail.com"; // Ваш e-mail
-    $subject = "Сообщение с сайта"; // Тема сообщения
-    $headers= "Adssmart\r\n";
-    $headers .= "Content-type: text/html; charset=utf-8\r\n";
-    $headers .= "From: adssmart.ru <adssmart.ru>\r\n";
-    if(mail($mailTo, $subject, $message, $headers)) {
+    $mailTo = "info@erpsmart.ru"; // Ваш e-mail
+    // $mailTo = "sedrin880@gmail.com";
+    $subject = "AdsSmart"; // Тема сообщения
+    if(send_mime_mail($mailfrom,
+    $mailfrom,
+    $mailTo,
+    $mailTo,
+    'UTF-8',  // кодировка, в которой находятся передаваемые строки
+    'KOI8-R', // кодировка, в которой будет отправлено письмо
+    $subject,
+    $message)) {
         echo "Спасибо за обращение!". "\n". "Мы свяжемся с вами в ближайшее время"; 
     } else {
         echo "Сообщение не отправлено!";
     }
+}
+?>
+
+<?
+    function send_mime_mail($name_from, // имя отправителя
+    $email_from, // email отправителя
+    $name_to, // имя получателя
+    $email_to, // email получателя
+    $data_charset, // кодировка переданных данных
+    $send_charset, // кодировка письма
+    $subject, // тема письма
+    $body, // текст письма
+    $html = FALSE, // письмо в виде html или обычного текста
+    $reply_to = FALSE
+    ) {
+$to = mime_header_encode($name_to, $data_charset, $send_charset)
+. ' <' . $email_to . '>';
+$subject = mime_header_encode($subject, $data_charset, $send_charset);
+$from =  mime_header_encode($name_from, $data_charset, $send_charset)
+ .' <' . $email_from . '>';
+if($data_charset != $send_charset) {
+    $body = iconv($data_charset, $send_charset . '//IGNORE', $body);
+}
+$headers = "From: $from\r\n";
+$type = ($html) ? 'html' : 'plain';
+$headers .= "Content-type: text/$type; charset=$send_charset\r\n";
+$headers .= "Mime-Version: 1.0\r\n";
+if ($reply_to) {
+$headers .= "Reply-To: $reply_to";
+}
+return mail($to, $subject, $body, $headers);
+}
+
+function mime_header_encode($str, $data_charset, $send_charset) {
+if($data_charset != $send_charset) {
+    $str = iconv($data_charset, $send_charset . '//IGNORE', $str);
+}
+return '=?' . $send_charset . '?B?' . base64_encode($str) . '?=';
 }
 ?>
